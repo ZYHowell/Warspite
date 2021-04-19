b15d92f027fa52a20c181c147fb0137d366dd09d
ZYHowell <zhuangyh@sjtu.edu.cn>
2021-04-20 00:19:43 +0800

    Guiding me home in the dark

--- builtin.s
+++ builtin.s
@@ -1,8 +1,5 @@
 	.file	"builtin.c"
 	.option nopic
-	.attribute arch, "rv32i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
-	.attribute unaligned_access, 0
-	.attribute stack_align, 16
 	.text
 	.section	.rodata.str1.4,"aMS",@progbits,1
 	.align	2
